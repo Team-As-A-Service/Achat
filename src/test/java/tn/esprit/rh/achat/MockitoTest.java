e223d04c58295cbfaaa767a9ad3a1a858e1c9cd5
ahmed smiai <ahmedsmiai3@gmail.com>
2023-10-10 15:17:00 +0100

    first commit

--- src/test/java/tn/esprit/rh/achat/MockitoTest.java
+++ src/test/java/tn/esprit/rh/achat/MockitoTest.java
@@ -1,3 +1,4 @@
+
 package tn.esprit.rh.achat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -74,20 +75,21 @@ public class MockitoTest {
 //            .andExpect((ResultMatcher) jsonPath("$[2].libelleStock", is("alimentaire")));
 //}
 
-@Test
+    @Test
     public void getStockTest(){
-    System.out.println(" get test stock");
-    long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-    long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        System.out.println(" get test stock");
+        long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
 
-    repository = mock(StockRepository.class);
-    service = new StockServiceImpl(repository);
+        repository = mock(StockRepository.class);
+        service = new StockServiceImpl(repository);
 
-    List<Stock> stockList = new ArrayList<>();
-    stockList.add(new Stock(id,"alimentaire",100,5));
-    stockList.add(new Stock(id2,"vest",50,10));
-when(repository.findAll()).thenReturn(stockList);
+        List<Stock> stockList = new ArrayList<>();
+        stockList.add(new Stock(id,"alimentaire",100,5));
+        stockList.add(new Stock(id2,"vest",50,10));
+        when(repository.findAll()).thenReturn(stockList);
+
+    }
 
 }
 
-}
--- src/test/java/tn/esprit/rh/achat/ProduitTest.java
+++ src/test/java/tn/esprit/rh/achat/ProduitTest.java
@@ -12,25 +12,24 @@ import tn.esprit.rh.achat.repositories.ProduitRepository;
 import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.ProduitServiceImpl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyFloat;
-import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 @ContextConfiguration(classes = {ProduitServiceImpl.class})
 @ExtendWith(SpringExtension.class)
 public class ProduitTest {
 
     @MockBean
-    private CategorieProduitRepository categorieProduitRepository;
+   private CategorieProduitRepository categorieProduitRepository;
 
     @MockBean
     private ProduitRepository produitRepository;
@@ -49,6 +48,7 @@ public class ProduitTest {
         assertTrue(actualRetrieveAllProduitsResult.isEmpty());
         verify(produitRepository).findAll();
     }
+
     @Test
     void testDeleteProduit() {
         doNothing().when(produitRepository).deleteById((Long) any());

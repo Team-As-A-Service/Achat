05cc18c13d20ec276dbd5152a8359d4ca5a05e86
BOULARES Mohamedridha <74650380+ridha-boulares@users.noreply.github.com>
2024-02-27 14:25:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,45 @@
-terraform {
-required_version = ">= 0.14.0"
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.53.0"
-    }
+resource "azurerm_resource_group" "aks-rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  location            = var.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  dns_prefix          = var.cluster_name
+
+  default_node_pool {
+    name                = "system"
+    node_count          = var.system_node_count
+    vm_size             = "Standard_DS2_v2"
+    type                = "VirtualMachineScaleSets"
+    zones  = [1, 2, 3]
+    enable_auto_scaling = false
   }
-}
 
-
-provider "openstack" {
-  user_name   = "admin"
-  tenant_name = "admin"
-  password    = "secret"
-  auth_url    = "http://198.168.22.121/identity"
-  region      = "RegionOne"
-}
-
-resource "openstack_compute_instance_v2" "Cirros" {
-  name            = "Cirros"
-  image_id        = "ce620d5a-360c-457a-a25a-f6496f0947f7"
-  flavor_id       = "c1"
-  key_pair        = "my_keypair"
-  security_groups = ["default"]
-  network {
-    name = "private"
+  identity {
+    type = "SystemAssigned"
   }
-}
 
-resource "openstack_compute_keypair_v2" "test-keypair" {
-  name = "my_keypair"
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet" 
+  }
 }
